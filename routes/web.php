13b944e2f48283e2b3de996f54fee0d1b127ceb9
Guilherme Victor <guilhermevr.figueiredo@outlook.com>
2024-08-31 13:37:12 -0300

    feat: adiciona sistema de verificação de usuários logados no sistema

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,20 @@ Route::get('/', function() {
 });
 
 Route::controller(AdministratorController::class)->group( function () {
-    Route::get('admin/menu', 'menu')->name('admin-menu');
+    Route::get('admin/menu', 'menu')->name('admin-menu')->middleware('auth');;
     Route::get('admin/login', 'index');
     Route::post('admin/authenticate',  'authenticate')->name('user.authenticate');
 });
 
 Route::controller(FootballTeamController::class)->group( function () {
-    Route::get('/team/create',  'create')->name('team.create');
-    Route::post('/team/store',  'store')->name('team.store');
-    Route::get('/team/ranking',  'ranking')->name('team.ranking');
+    Route::get('/team/create',  'create')->name('team.create')->middleware('auth');;
+    Route::post('/team/store',  'store')->name('team.store')->middleware('auth');;
+    Route::get('/team/ranking',  'ranking')->name('team.ranking')->middleware('auth');;
 });
 
 Route::controller(UserController::class)->group( function () {
     Route::get('/signup', 'index');
-    Route::get('/menu', 'menu')->name('user.menu');
+    Route::get('/menu', 'menu')->name('user.menu')->middleware('auth');;
     Route::post('store',  'store')->name('user.store');
 });
 
@@ -39,31 +39,31 @@ Route::controller(LoginController::class)->group( function () {
 });
 
 Route::controller(FootballPlayerController::class)->group( function () {
-    Route::get('/player/create',  'create')->name('player.create');
-    Route::get('/players',  'index')->name('listAllPlayers');
-    Route::post('/players',  'index')->name('listAllPlayers');
-    Route::post('/player/store',  'store')->name('player.store');
+    Route::get('/player/create',  'create')->name('player.create')->middleware('auth');;
+    Route::get('/players',  'index')->name('listAllPlayers')->middleware('auth');;
+    Route::post('/players',  'index')->name('listAllPlayers')->middleware('auth');;
+    Route::post('/player/store',  'store')->name('player.store')->middleware('auth');;
 })->middleware('auth.basic');
 
 Route::controller(TeamPlayerController::class)->group( function () {
     Route::get('/teamPlayer/create',  'create')->name('teamPlayer.create');
-    Route::get('/teamPlayer/ranking',  'ranking')->name('teamPlayer.ranking');
-    Route::post('/teamPlayer/ranking',  'ranking')->name('teamPlayer.ranking');
+    Route::get('/teamPlayer/ranking',  'ranking')->name('teamPlayer.ranking')->middleware('auth');;
+    Route::post('/teamPlayer/ranking',  'ranking')->name('teamPlayer.ranking')->middleware('auth');;
     Route::post('/teamPlayer/store',  'store')->name('teamPlayer.store');
 });
 
 Route::controller(RoundController::class)->group( function () {
-    Route::get('/round', 'index')->name('round.index');
-    Route::post('/finishRound', 'finishRound')->name('round.finish');
+    Route::get('/round', 'index')->name('round.index')->middleware('auth');;
+    Route::post('/finishRound', 'finishRound')->name('round.finish')->middleware('auth');;
 });
 
 Route::controller(RoundLineupController::class)->group( function () {
-    Route::post('/roundLineup/store',  'store')->name('roundLineup.store');
-    Route::get('roundLineup', 'index')->name('roundLineup');
-    Route::post('/roundLineup/delete', 'destroy')->name('roundLine.destroy');
+    Route::post('/roundLineup/store',  'store')->name('roundLineup.store')->middleware('auth');;
+    Route::get('roundLineup', 'index')->name('roundLineup')->middleware('auth');;
+    Route::post('/roundLineup/delete', 'destroy')->name('roundLine.destroy')->middleware('auth');;
 });
 
 Route::controller(FootballPlayerScoreController::class)->group( function () {
-    Route::get('/footaballPlayerScore',  'index')->name('footaballPlayerScore.index');
-    Route::post('/footaballPlayerScore',  'index')->name('footaballPlayerScore.index');
+    Route::get('/footaballPlayerScore',  'index')->name('footaballPlayerScore.index')->middleware('auth');;
+    Route::post('/footaballPlayerScore',  'index')->name('footaballPlayerScore.index')->middleware('auth');;
 });
